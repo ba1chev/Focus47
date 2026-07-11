afc4eaae69de5cdf28bfcc800f1b4b368ab39d9d
ba1chev <baychevyoan@gmail.com>
2026-07-11 16:24:41 +0300

    Added TaskUpdate model for partial task updates

---
+++ app/models/tasks/task_update.py
@@ -0,0 +1,17 @@
+from typing import Optional
+from pydantic import BaseModel
+
+from app.models.enums.status import Status
+from app.models.enums.priority import Priority
+
+
+class TaskUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    start: Optional[str] = None
+    end: Optional[str] = None
+    status: Optional[Status] = None
+    priority: Optional[Priority] = None
+    category: Optional[str] = None
+    color: Optional[str] = None
+    user_id: Optional[int] = None
